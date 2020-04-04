0766d3e322dce2b10e78534c52797d42b04dd3f9
William Warren <willwarrenmail@gmail.com>
2020-04-04 14:22:26 -0400

    got a start on jumpin needs work but jumping works

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -6,17 +6,24 @@ public class player : KinematicBody2D
     [Export] public int speed = 100;
     [Signal] public delegate void _BloodSeeker();
     public Vector2 velocity = new Vector2();
+    public int jumpVelocity = 3;
+    public int movementModifier = 0;
+
+    public bool isJumping = false;
+    public bool isFalling = false;
+
+    public float jumpStartY = 0;
 
     public void GetInput()
     {
         velocity = new Vector2();
-        string direction = string.Empty;
         AnimationPlayer animation = (AnimationPlayer)GetNode("anim");
+        string direction = animation.CurrentAnimation;
+
         if (Input.IsActionPressed("right"))
         {
             velocity.x += 1;
             animation.Play("Walkright");
-            direction = "right";
         }
         if (Input.IsActionJustReleased("right"))
         {
@@ -27,7 +34,6 @@ public class player : KinematicBody2D
         {
             velocity.x -= 1;
             animation.Play("Walkleft");
-            direction = "left";
         }
         if (Input.IsActionJustReleased("left"))
         {
@@ -38,7 +44,6 @@ public class player : KinematicBody2D
         {
             velocity.y += 1;
             animation.Play("Walkdown");
-            direction = "down";
         }
         if (Input.IsActionJustReleased("down"))
         {
@@ -49,12 +54,88 @@ public class player : KinematicBody2D
         {
             velocity.y -= 1;
             animation.Play("Walkup");
-            direction = "up";
         }
         if (Input.IsActionJustReleased("up"))
         {
             animation.Stop();
             animation.Play("Idleup");
+        }
+
+        // JUMPING
+        if (Input.IsActionPressed("jump"))
+        {
+            if (!isJumping)
+            {
+                jumpStartY = Position.y;
+            }
+            isJumping = true;
+            movementModifier = jumpVelocity;
+
+            if (direction.Contains("up"))
+            {
+                velocity.y -= movementModifier;
+            }
+
+            if (direction.Contains("right"))
+            {
+                velocity.y -= movementModifier;
+                velocity.x += jumpVelocity;
+            }
+
+            if (direction.Contains("down"))
+            {
+
+                velocity.y += movementModifier;
+
+            }
+            if (direction.Contains("left"))
+            {
+
+                velocity.y -= movementModifier;
+                velocity.x -= jumpVelocity;
+            }
+        }
+        else if (Input.IsActionJustReleased("jump"))
+        {
+            Console.WriteLine("released jump");
+            isJumping = false;
+            isFalling = true;
+        }
+        else if (isFalling)
+        {
+            Console.WriteLine("isFalling jumpStartY =" + jumpStartY);
+            if (Position.y < jumpStartY)
+            {
+                Console.WriteLine("returning to position");
+                if (direction.Contains("up"))
+                {
+                    velocity.y -= -movementModifier;
+                }
+
+                if (direction.Contains("right"))
+                {
+                    velocity.y -= -movementModifier;
+                    velocity.x += jumpVelocity;
+                }
+
+                if (direction.Contains("down"))
+                {
+
+                    velocity.y += -movementModifier;
+
+                }
+                if (direction.Contains("left"))
+                {
+
+                    velocity.y -= -movementModifier;
+                    velocity.x -= jumpVelocity;
+                }
+            }
+            else if (Position.y >= jumpStartY)
+            {
+                Console.WriteLine("Landed");
+                isFalling = false;
+            }
         }
 
         for (int i = 0; i < GetSlideCount(); i++)
